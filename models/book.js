323a54b7a8eabc0c2a4b5a21372f544c0e6403b3
FionaViola <aditi.nadig10@gmail.com>
2018-12-14 19:08:42 -0500

    added models

---
+++ models/book.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    authors: {
+        type: String,
+        required: true
+    },
+
+    summary: String,
+
+    image: String,
+
+    link: {
+        type: String,
+        required: true
+    },
+
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
